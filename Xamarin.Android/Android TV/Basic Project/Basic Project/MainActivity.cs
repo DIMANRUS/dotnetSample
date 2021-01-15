583341d430c597b580fd26e75cae230aff13b652
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2021-01-15 17:57:42 +0300

    Добавьте файлы проекта.

---
+++ Xamarin.Android/Android TV/Basic Project/Basic Project/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace Basic_Project
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, Name = "com.dimanrus.activity")]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
