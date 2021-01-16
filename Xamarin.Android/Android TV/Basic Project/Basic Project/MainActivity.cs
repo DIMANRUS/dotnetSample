79f4b3bfd76160478c96c2cdb7ad3a5c46591afe
DIMANRUS <41239551+DIMANRUS@users.noreply.github.com>
2021-01-16 16:06:17 +0300

    Upload project. Add splash, icon, banner

--- Xamarin.Android/Android TV/Basic Project/Basic Project/MainActivity.cs
+++ Xamarin.Android/Android TV/Basic Project/Basic Project/MainActivity.cs
@@ -2,11 +2,9 @@
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
-using Android.Widget;
 
-namespace Basic_Project
-{
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, Name = "com.dimanrus.activity")]
+namespace Basic_Project {
+    [Activity(Theme = "@style/AppTheme", Name = "com.dimanrus.activity")]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -14,9 +12,7 @@ namespace Basic_Project
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)=>
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
     }
 }
---
+++ Xamarin.Android/Android TV/Basic Project/Basic Project/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Basic_Project {
+    [Activity(MainLauncher = true, Name = "com.dimanrus.activitysplash", NoHistory = true)]
+    public class SplashActivity : AppCompatActivity {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState) =>
+            base.OnCreate(savedInstanceState, persistentState);
+        protected override void OnResume() {
+            base.OnResume();
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
